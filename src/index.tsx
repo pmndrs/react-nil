cbbfccae508617c4b34e15494609565a1bc73e7d
Cody Bennett <hi@codyb.co>
2024-05-15 14:31:18 -0500

    chore: add shell types for 19

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,103 @@
 import { version, type ReactNode, type JSX } from 'react'
 import Reconciler from 'react-reconciler'
-// @ts-ignore
 import { NoEventPriority, DefaultEventPriority, ConcurrentRoot } from 'react-reconciler/constants.js'
+
+// TODO: upstream to DefinitelyTyped for React 19
+// https://github.com/facebook/react/issues/28956
+type EventPriority = number
+
+const createReconciler = Reconciler as unknown as <
+  Type,
+  Props,
+  Container,
+  Instance,
+  TextInstance,
+  SuspenseInstance,
+  HydratableInstance,
+  FormInstance,
+  PublicInstance,
+  HostContext,
+  ChildSet,
+  TimeoutHandle,
+  NoTimeout,
+  TransitionStatus,
+>(
+  config: Omit<
+    Reconciler.HostConfig<
+      Type,
+      Props,
+      Container,
+      Instance,
+      TextInstance,
+      SuspenseInstance,
+      HydratableInstance,
+      PublicInstance,
+      HostContext,
+      null, // updatePayload
+      ChildSet,
+      TimeoutHandle,
+      NoTimeout
+    >,
+    'getCurrentEventPriority' | 'prepareUpdate' | 'commitUpdate'
+  > & {
+    /**
+     * This method should mutate the `instance` and perform prop diffing if needed.
+     *
+     * The `internalHandle` data structure is meant to be opaque. If you bend the rules and rely on its internal fields, be aware that it may change significantly between versions. You're taking on additional maintenance risk by reading from it, and giving up all guarantees if you write something to it.
+     */
+    commitUpdate?(
+      instance: Instance,
+      type: Type,
+      prevProps: Props,
+      nextProps: Props,
+      internalHandle: Reconciler.OpaqueHandle,
+    ): void
+
+    // Undocumented
+    // https://github.com/facebook/react/pull/26722
+    NotPendingTransition: TransitionStatus | null
+    // https://github.com/facebook/react/pull/28751
+    setCurrentUpdatePriority(newPriority: EventPriority): void
+    getCurrentUpdatePriority(): EventPriority
+    resolveUpdatePriority(): EventPriority
+    // https://github.com/facebook/react/pull/28804
+    resetFormInstance(form: FormInstance): void
+    // https://github.com/facebook/react/pull/25105
+    requestPostPaintCallback(callback: (time: number) => void): void
+    // https://github.com/facebook/react/pull/26025
+    shouldAttemptEagerTransition(): boolean
+
+    /**
+     * This method is called during render to determine if the Host Component type and props require some kind of loading process to complete before committing an update.
+     */
+    maySuspendCommit(type: Type, props: Props): boolean
+    /**
+     * This method may be called during render if the Host Component type and props might suspend a commit. It can be used to initiate any work that might shorten the duration of a suspended commit.
+     */
+    preloadInstance(type: Type, props: Props): boolean
+    /**
+     * This method is called just before the commit phase. Use it to set up any necessary state while any Host Components that might suspend this commit are evaluated to determine if the commit must be suspended.
+     */
+    startSuspendingCommit(): void
+    /**
+     * This method is called after `startSuspendingCommit` for each Host Component that indicated it might suspend a commit.
+     */
+    suspendInstance(type: Type, props: Props): void
+    /**
+     * This method is called after all `suspendInstance` calls are complete.
+     *
+     * Return `null` if the commit can happen immediately.
+     *
+     * Return `(initiateCommit: Function) => Function` if the commit must be suspended. The argument to this callback will initiate the commit when called. The return value is a cancellation function that the Reconciler can use to abort the commit.
+     *
+     */
+    waitForCommitToBeReady(): ((initiateCommit: Function) => Function) | null
+  },
+) => Reconciler.Reconciler<Container, Instance, TextInstance, SuspenseInstance, PublicInstance>
+
+declare module 'react-reconciler/constants.js' {
+  const NoEventPriority = 0
+}
 
 export interface NilNode<P = Record<string, unknown>> {
   type: string
@@ -21,12 +117,13 @@ interface HostConfig {
   textInstance: NilNode
   suspenseInstance: NilNode
   hydratableInstance: never
+  formInstance: never
   publicInstance: null
   hostContext: {}
-  updatePayload: {}
   childSet: never
   timeoutHandle: number
   noTimeout: -1
+  TransitionStatus: null
 }
 
 // react-reconciler exposes some sensitive props. We don't want them exposed in public instances
@@ -45,7 +142,7 @@ const NO_CONTEXT: HostConfig['hostContext'] = {}
 
 let currentUpdatePriority: number = NoEventPriority
 
-const reconciler = Reconciler<
+const reconciler = createReconciler<
   HostConfig['type'],
   HostConfig['props'],
   HostConfig['container'],
@@ -53,12 +150,13 @@ const reconciler = Reconciler<
   HostConfig['textInstance'],
   HostConfig['suspenseInstance'],
   HostConfig['hydratableInstance'],
+  HostConfig['formInstance'],
   HostConfig['publicInstance'],
   HostConfig['hostContext'],
-  HostConfig['updatePayload'],
   HostConfig['childSet'],
   HostConfig['timeoutHandle'],
-  HostConfig['noTimeout']
+  HostConfig['noTimeout'],
+  HostConfig['TransitionStatus']
 >({
   isPrimaryRenderer: false,
   supportsMutation: true,
@@ -95,9 +193,8 @@ const reconciler = Reconciler<
   beforeActiveInstanceBlur() {},
   afterActiveInstanceBlur() {},
   detachDeletedInstance() {},
-  // @ts-ignore untyped react-experimental options inspired by react-art
-  // TODO: add shell types for these and upstream to DefinitelyTyped
-  // https://github.com/facebook/react/blob/main/packages/react-art/src/ReactFiberConfigART.js
+  prepareScopeUpdate() {},
+  getInstanceFromScope: () => null,
   setCurrentUpdatePriority(newPriority) {
     currentUpdatePriority = newPriority
   },
